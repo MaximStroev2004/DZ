2c82c437a762adc9f7904460cb0e26d2458c8845
MaximStroev2004 <120455884+MaximStroev2004@users.noreply.github.com>
2023-05-12 16:12:18 +0300

    Create MirCard.java

---
+++ MirCard.java
@@ -0,0 +1,18 @@
+package com.example.demo4;
+
+public class MirCard extends Card {
+
+    public MirCard() {
+        super(PaymentSystem.MIR);
+    }
+
+    @Override
+     public void payInCountry(Country country, int amount) {
+
+    }
+    public boolean isCountryValidForTheseCard(Country country) {
+        if (country == Country.RU)
+            return true;
+        else
+            return false;
+}
